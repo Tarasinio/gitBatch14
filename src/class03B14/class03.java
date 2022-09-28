f6d7b812c17cfe269500e3ae6f2e4c18857d1bed
username <abc.123@gmail.com>
2022-09-28 17:25:27 +0300

    second commit

--- src/class03B14/class03.java
+++ src/class03B14/class03.java
@@ -4,7 +4,8 @@ public class class03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		 System.out.println("hey this is a git class");
+		 System.out.println("this is a change in my project");
 	}
 
 }
